5302f390d8f1141e5d1957cf9216a3c34e1c23c1
Arsen Muk <arsenmuk@clickhouse.com>
2026-07-06 14:01:19 +0200

    Revised implementation to fix background operations

--- src/Storages/MergeTree/MutateTask.cpp
+++ src/Storages/MergeTree/MutateTask.cpp
@@ -2256,7 +2256,7 @@ private:
             if (!entries_to_hardlink.contains(proj->name()))
                 continue;
 
-            ctx->new_data_part->getDataPartStorage().createProjection(proj->name());
+            ctx->new_data_part->getDataPartStorage().createProjection(proj->name(), ctx->new_data_part->getProjectionStorageFormat());
 
             auto projection_data_part_storage_src = ctx->source_part->getDataPartStorage().getProjection(proj->name());
             auto projection_data_part_storage_dst = ctx->new_data_part->getDataPartStorage().getProjection(proj->name());
@@ -2564,7 +2564,7 @@ private:
             if (rename_it != ctx->files_to_rename.end())
                 continue;
 
-            ctx->new_data_part->getDataPartStorage().createProjection(projection_dir);
+            ctx->new_data_part->getDataPartStorage().createProjection(projection_dir, ctx->new_data_part->getProjectionStorageFormat());
 
             auto projection_data_part_storage_src = ctx->source_part->getDataPartStorage().getProjection(projection_dir);
             auto projection_data_part_storage_dst = ctx->new_data_part->getDataPartStorage().getProjection(projection_dir);
